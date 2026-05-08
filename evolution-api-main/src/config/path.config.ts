ff3f12e36198b5b343960e67f50f0341ad4b3ed8
Lu-Luou <rubertolh07@gmail.com>
2026-05-08 13:36:31 -0300

    fix compose + path /sessions

--- evolution-api-main/src/config/path.config.ts
+++ evolution-api-main/src/config/path.config.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 
 export const ROOT_DIR = process.cwd();
-export const INSTANCE_DIR = join(ROOT_DIR, 'instances');
+export const INSTANCE_DIR = process.env.INSTANCE_DIR || process.env.RAILWAY_VOLUME_MOUNT_PATH || '/sessions';
 export const SRC_DIR = join(ROOT_DIR, 'src');
 export const AUTH_DIR = join(ROOT_DIR, 'store', 'auth');
 export const STORE_DIR = join(ROOT_DIR, 'store');
